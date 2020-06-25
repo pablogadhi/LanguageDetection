8efbfb4fb528508978420bbfdf02386ede1a2bc7
Gadhi <pablogadhi@gmail.com>
2020-06-25 17:20:49 -0600

    Validation steps param separated

--- onmt_train.sh
+++ onmt_train.sh
@@ -3,13 +3,19 @@ data_dir="./data/*"
 export CUDA_VISIBLE_DEVICES=0
 
 steps=$3
-batch_size=$4
-save_at=$5
-checkpoint=$6
+val_steps=$4
+batch_size=$5
+save_at=$6
+checkpoint=$7
 
 if [ -z "$steps" ]
 then
     steps=200000
+fi
+
+if [ -z "$val_steps" ]
+then
+    steps=10000
 fi
 
 if [ -z "$batch_size" ]
@@ -29,5 +35,5 @@ onmt_train -data $1 -save_model $2 \
         -batch_size $batch_size -batch_type tokens -normalization tokens  -accum_count 2 \
         -optim adam -adam_beta2 0.998 -decay_method noam -warmup_steps 8000 -learning_rate 2 \
         -max_grad_norm 0 -param_init 0  -param_init_glorot \
-        -label_smoothing 0.1 -valid_steps $save_at -save_checkpoint_steps $save_at \
-        -world_size 1 -gpu_ranks 0 -train_from "$6"
+        -label_smoothing 0.1 -valid_steps $val_steps -save_checkpoint_steps $save_at \
+        -world_size 1 -gpu_ranks 0 -train_from "$8"
