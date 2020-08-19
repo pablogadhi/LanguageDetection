0aac1c7e1c087088235b7702cbd462723aa5246a
Gadhi <pablogadhi@gmail.com>
2020-08-19 10:25:44 -0600

    New detector strategies and training

--- detector.py
+++ detector.py
@@ -1,4 +1,7 @@
+import re
+import time
 import numpy as np
+import torch
 from torch import nn
 from torch import optim
 from torch.utils.data import Dataset, DataLoader
@@ -13,52 +16,95 @@ class BLEUDataset(Dataset):
     def __len__(self):
         return len(self.dataframe.index)
 
+    def __getitem__(self, idx):
+        inputs = self.dataframe.iloc[idx,
+                                     self.dataframe.columns != 'origin'].values
+        label = int(self.dataframe.iloc[idx]['origin'])
+        return inputs, label
+
+    def get_all_labels(self):
+        return [int(x['origin']) for _, x in self.dataframe.iterrows()]
+
 
 class Classifier(nn.Module):
     def __init__(self, dropout=0.5):
         super(Classifier, self).__init__()
-        self.hidden = nn.Linear(17, 9, bias=True)
-        self.relu = nn.ReLU()
+        self.hidden0 = nn.Linear(13, 18, bias=True)
+        self.hidden1 = nn.Linear(18, 9, bias=True)
         self.out = nn.Linear(9, 4, bias=True)
-        self.softmax = nn.Softmax(4)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)
         self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x):
-        x = self.relu(self.hidden(x))
+        x = self.relu(self.hidden0(x))
+        x = self.dropout(x)
+        x = self.relu(self.hidden1(x))
         x = self.dropout(x)
         x = self.softmax(self.out(x))
         return x
 
 
-def train_classifier(classifier, dataloader):
+def train_classifier(classifier, dataloader, epochs, log_rate, device='cpu'):
+    classifier.train()
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(classifier.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(classifier.parameters(), lr=0.01, momentum=0.9)
 
-    for epoch in range(2):
+    for epoch in range(epochs):
         running_loss = 0.0
+        start = time.time()
+        total, correct = (0, 0)
         for i, data in enumerate(dataloader, 0):
-            inputs, labels = data
+            inputs, labels = data[0].to(device), data[1].to(device)
             optimizer.zero_grad()
 
-            outputs = classifier(inputs)
+            outputs = classifier(inputs.float())
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
 
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
             running_loss += loss.item()
-            if i % 2000 == 1999:
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
+            if i % log_rate == log_rate - 1:
+                print('Epoch {}, iter {}: {:.4f} loss | {:.4f}% accuracy'.format(
+                    epoch, i + 1, running_loss / log_rate, correct / total * 100))
                 running_loss = 0.0
+
+        end = time.time()
+        print('Epoch {} took: {:.3f}s'.format(epoch, end - start))
 
     print('Finished Training')
 
 
-class Detector:
-    def __init__(self):
-        self.tokenizer = Tokenizer('conservative')
-        self.classifier = self.load_classifier()
+def test_classifier(classifier, dataloader):
+    classifier.eval()
 
-    def load_classifier(self):
-        # TODO Load a trained model
-        return Classifier()
+    predictions = []
+    total, correct = (0, 0)
+    for data in dataloader:
+        inputs, labels = data
+        outputs = classifier(inputs.float())
+
+        _, predicted = torch.max(outputs.data, 1)
+        predictions = predictions + predicted.tolist()
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the test data: {:.2f}%'.format(
+        100 * correct / total))
+    return predictions
+
+
+class Detector:
+    def __init__(self, model_path):
+        self.tokenizer = Tokenizer('conservative')
+        self.classifier = self.load_classifier(model_path)
+
+    def load_classifier(self, model_path):
+        classifier = Classifier()
+        classifier.load_state_dict(torch.load(model_path))
+        classifier.eval()
+        return classifier
