0b4ba1e41d191c6aa05700b16d650e36c8038aa4
Gadhi <pablogadhi@gmail.com>
2020-09-19 21:54:01 -0600

    Added parameters to classifier translation script

--- classifier_translations_generator.sh
+++ classifier_translations_generator.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+inputDir=$1
+outputDir=$2
+
 inputLangs=("es" "fr")
 outputLangs=("es" "fr" "en" "de")
 
 for input in ${inputLangs[@]}; do
     for output in ${outputLangs[@]}; do
         if [ "$input" != "$output" ]; then
-            python tools/google_translator.py --source "data/aligned/classifier/${input}.txt" --output "data/translated/${input}_${output}.txt" --tgt "${output}"
+            python tools/google_translator.py --source "${inputDir}/${input}.txt" --output "${outputDir}/${input}_${output}.txt" --tgt "${output}"
         fi
     done
 done
